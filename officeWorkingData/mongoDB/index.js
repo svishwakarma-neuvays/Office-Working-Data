0d8b8a5f8ccea62118c0bc35c6425bf1e9ad405e
unknown <svishwakarma@neuvays.com>
2024-04-22 21:09:31 +0530

    add

--- officeWorkingData/mongoDB/index.js
+++ officeWorkingData/mongoDB/index.js
@@ -1,7 +1,34 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017").then(()=>{
+mongoose
+  .connect("mongodb://127.0.0.1:27017/sample")
+  .then(() => {
     console.log("connected to mongoDB!!");
-}).catch((err)=>{
+  })
+  .catch((err) => {
     console.log(err);
-})
+  });
+
+const student = new mongoose.Schema({
+  name: { type: String, require: true },
+  workout: Boolean,
+  height: Number,
+});
+
+const Student = new mongoose.model("Student", student);
+
+const adder = async () => {
+  //   const ss = await Student.create({
+  //     name: "aa",
+  //     workout: false,
+  //     height: 44,
+  //   });
+//   const ss = await Student.find({height:{$eq:44}});
+//   const ss = await Student.find({height:{$gte:5}});
+//   const ss = await Student.find({height:{$lte:5}});
+//   const ss = await Student.find({height:{$in:[5,44]}});
+  const ss = await Student.find({height:{$nin:[5,44]}});
+  console.log(ss);
+};
+
+adder();
